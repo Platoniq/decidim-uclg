d683d4788a0a79af13a1f35888f25f42db521c61
Ivan Vergés <ivan@platoniq.net>
2020-07-15 20:04:50 +0200

    init decidim app

---
+++ Gemfile
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+ruby RUBY_VERSION
+
+DECIDIM_VERSION = { git: "https://github.com/decidim/decidim", branch: "0.21-stable" }
+
+gem "decidim", DECIDIM_VERSION
+# gem "decidim-consultations", DECIDIM_VERSION
+# gem "decidim-initiatives", DECIDIM_VERSION
+
+gem "bootsnap", "~> 1.4"
+gem "health_check"
+gem "sidekiq", "~> 6.0"
+gem "sidekiq-cron"
+gem "sentry-raven"
+
+gem "puma", "~> 4.3"
+gem "uglifier", "~> 4.1"
+
+gem "faker", "~> 1.9"
+
+group :development, :test do
+  gem "byebug", "~> 11.0", platform: :mri
+
+  gem "decidim-dev", DECIDIM_VERSION
+end
+
+group :development do
+  gem "letter_opener_web", "~> 1.3"
+  gem "listen", "~> 3.1"
+  gem "spring", "~> 2.0"
+  gem "spring-watcher-listen", "~> 2.0"
+  gem "web-console", "~> 3.5"
+end
+
+
+group :production do
+  gem "fog-aws"
+end
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Default CarrierWave setup.
+#
+CarrierWave.configure do |config|
+  config.permissions = 0o666
+  config.directory_permissions = 0o777
+  config.storage = :file
+  config.enable_processing = !Rails.env.test?
+end
+
+if Rails.application.secrets.aws_access_key_id.present?
+  require "carrierwave/storage/fog"
+
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region:                'eu-west-1',
+      host:                  's3.eu-west-1.amazonaws.com',
+      # endpoint:              'https://s3.example.com:8080'
+    }
+    config.fog_directory  = ENV.fetch("AWS_BUCKET_NAME", 'testparticipa')
+    # config.fog_public     = false
+    config.fog_attributes = {
+      'Cache-Control' => "max-age=#{365.day.to_i}",
+      'X-Content-Type-Options' => "nosniff"
+    }
+  end
+end
