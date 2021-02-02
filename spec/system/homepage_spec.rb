9efed12afccf974b59eed26ade77b55901f267e0
Vera Rojman <vrojman@protonmail.com>
2021-02-02 18:28:22 +0100

    Fix matomo spec

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -7,15 +7,14 @@ describe "Visit the home page", type: :system, perform_enqueued: true do
 
   before do
     switch_to_host(organization.host)
+    visit decidim.root_path
   end
 
   it "renders the home page" do
-    visit decidim.root_path
     expect(page).to have_content("Home")
   end
 
   it "has the proper background color" do
-    visit decidim.root_path
     expect(page.execute_script("return window.getComputedStyle($('.title-bar')[0]).backgroundColor")).to eq("rgb(87, 87, 86)")
     expect(page.execute_script("return window.getComputedStyle($('.main-footer')[0]).backgroundColor")).to eq("rgb(87, 87, 86)")
   end
