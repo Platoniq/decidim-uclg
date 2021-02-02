227779895efbed657e762306d269dd461988436b
Vera Rojman <vera@platoniq.net>
2021-02-02 19:33:23 +0100

    Merge staging (#27)
    * remove update wkhtml config
    
    * Upgrade to 0.23 (#24)
    
    * upgrade to 0.23
    
    * fix migrations for old data
    
    * fix some tests
    
    * Update overriden conference files
    
    * Update overriden files
    
    * Fix video spec
    
    * Fix background
    
    * Fix matomo spec
    
    Co-authored-by: Vera Rojman <vrojman@protonmail.com>
    
    * Soundcloud widget (#26)
    
    * Add soundcloud widget
    
    * [WIP] Add spec for soundcloud embed
    
    * Reorganize specs
    
    * Make widget smaller
    
    * Improve spec
    
    * Fix procfile error
    
    Co-authored-by: David <david.igon@riseup.net>
    Co-authored-by: Ivan Vergés <ivan@platoniq.net>

---
+++ spec/system/conferences/program_page_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "The conference program page", type: :system, perform_enqueued: true do
+  let(:organization) { create :organization }
+  let(:slug) { "test-conference" }
+  let!(:conference) { create :conference, slug: slug, organization: organization }
+  let(:conference_settings) { Rails.application.secrets.dig(:uclg, :conferences).find { |conference| conference[:slug] == slug } }
+
+  let!(:component) do
+    create(:component, manifest_name: :meetings, participatory_space: conference)
+  end
+
+  let!(:meeting) { create(:meeting, component: component) }
+
+  context "when visiting the defined conference" do
+    before do
+      switch_to_host(organization.host)
+      visit decidim_conferences.conference_conference_program_path(conference, component)
+    end
+
+    describe "the background" do
+      it "has an image" do
+        element = page.find("main .wrapper")
+        expect(element.style("background-image")["background-image"]).to match(conference_settings[:program_page_background])
+      end
+    end
+
+    describe "the soundcloud widget" do
+      it "is rendered" do
+        expect(page).to have_selector("#soundcloudEmbed")
+        expect(page.find("#soundcloudEmbed")[:src]).to eq(conference_settings[:soundcloud_url])
+      end
+    end
+  end
+
+  context "when visiting another conference" do
+    let!(:conference_2) { create :conference, organization: organization }
+    let!(:component_2) do
+      create(:component, manifest_name: :meetings, participatory_space: conference_2)
+    end
+
+    let!(:meeting_2) { create(:meeting, component: component_2) }
+
+    before do
+      switch_to_host(organization.host)
+      visit decidim_conferences.conference_conference_program_path(conference_2, component_2)
+    end
+
+    describe "the background" do
+      it "has no image" do
+        element = page.find("main .wrapper")
+        expect(element.style("background-image")).to eq("background-image" => "none")
+      end
+    end
+
+    describe "the soundcloud widget" do
+      it "is not rendered" do
+        expect(page).to have_no_selector("#soundcloudEmbed")
+      end
+    end
+  end
+end
--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -7,16 +7,20 @@ describe "Visit the home page", type: :system, perform_enqueued: true do
 
   before do
     switch_to_host(organization.host)
+    visit decidim.root_path
   end
 
   it "renders the home page" do
-    visit decidim.root_path
     expect(page).to have_content("Home")
   end
 
   it "has the proper background color" do
-    visit decidim.root_path
     expect(page.execute_script("return window.getComputedStyle($('.title-bar')[0]).backgroundColor")).to eq("rgb(87, 87, 86)")
     expect(page.execute_script("return window.getComputedStyle($('.main-footer')[0]).backgroundColor")).to eq("rgb(87, 87, 86)")
+  end
+
+  it "has matomo tracker" do
+    expect(page.execute_script("return typeof window._paq")).not_to eq("undefined")
+    expect(page.execute_script("return typeof window._paq")).to eq("object")
   end
 end
