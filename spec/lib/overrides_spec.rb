03eb9214faab4ea56aac4b795661000034fa6309
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-02-10 14:45:21 +0100

    Fix direct verifications module

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -47,6 +47,17 @@ checksums = [
       "/app/presenters/decidim/debates/official_author_presenter.rb" => "f47ad586da31ff30ad853e170c3b773f"
       # classes
     }
+  }, {
+    package: "decidim-direct_verifications",
+    files: {
+      # The only change for controllers is the full namespace for the parent class as it didn't resolved it well when it
+      # was just ApplicationController
+      "/app/controllers/decidim/direct_verifications/verification/admin/authorizations_controller.rb" => "ec24f7eb75ad7ab298ef13a956873cf6",
+      "/app/controllers/decidim/direct_verifications/verification/admin/direct_verifications_controller.rb" => "4f9cef25f72bb5ce88480850bd3f162a",
+      "/app/controllers/decidim/direct_verifications/verification/admin/imports_controller.rb" => "477a63f3c749de204ccdc0987cd6b20d",
+      "/app/controllers/decidim/direct_verifications/verification/admin/stats_controller.rb" => "a0c4ae48b1372ea5d37aae0112c9c826",
+      "/app/controllers/decidim/direct_verifications/verification/admin/user_authorizations_controller.rb" => "c0f3387a8b76ecdf238e12e6c03daf3e"
+    }
   }
 ]
 
