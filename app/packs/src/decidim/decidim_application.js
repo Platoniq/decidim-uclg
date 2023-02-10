8a71be006d41dd44a45dfd8a9e8ded5a6d295873
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-02-10 11:35:43 +0100

    Remove useless import

--- app/packs/src/decidim/decidim_application.js
+++ app/packs/src/decidim/decidim_application.js
@@ -1,8 +1,6 @@
 // This file is compiled inside Decidim core pack. Code can be added here and will be executed
 // as part of that pack
 
-import "./floating_help.js";
-
 // Load images
 require.context("../../images", true)
 require.context("../../fonts", true)
